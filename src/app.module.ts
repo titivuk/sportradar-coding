36557f21b53867814900df5e5b483b1d9c717ce3
Kanstantsin Tatarchuk <titivuk@gmail.com>
2023-10-01 13:29:32 +0200

    remove nestjs sample code

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { SimulationModule } from './simulation/simulation.module';
 
 @Module({
   imports: [SimulationModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
